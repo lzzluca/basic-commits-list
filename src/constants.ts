7df56a82aea8158f25c3f7b692af75833a802690
Luca Lazzarini <luca@eclecticiq.com>
2022-08-07 17:58:48 +0200

    Working on pagination: fixed api to fetch by page and added the fetch for total commits count

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
+/*
 export const API_URL = '/repos/lzzlucareadonly/my-pet-project/commits';
 // this token is from a "created ad-hoc account" that accesses the repo by a fork: if the token is stolen, only
-// the second account is impacted
+// the second account is impacted. that brings to zero the risk of damages from storing the token here
 export const TOKEN = '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'; 
+*/
+
+// this is for debug: Vue is a big project with many commits, good to work on fetching commits by pagination
+export const API_URL = '/repos/vuejs/vue/commits';
--- src/features/commits/commits-thunk.ts
+++ src/features/commits/commits-thunk.ts
@@ -2,15 +2,31 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import { Octokit } from '@octokit/core';
 import { API_URL, TOKEN } from '../../constants';
 
+// returns a list of commits for the specified page, for master; there are max 30 commits per page
 export const fetchCommits = createAsyncThunk(
   'commits/fetchAllCommits',
-  async () => {
-    // todo: paginate this! it could be maaaany commits available
-    const result = await new Octokit().request(`GET ${API_URL}`, {
+  async (pageNumber: number = 1) => {
+    const response = await new Octokit().request(`GET ${API_URL}?page=${pageNumber}`, {
       headers: {
         authorization: TOKEN
-      },
-    })
-    
-    return result.data;
-});
+      }
+    });
+
+    return response.data;
+  }
+);
+
+// returns the total amount of commits for master
+export const fetchTotalCountCommits = createAsyncThunk(
+    'commits/fetchTotalCountCommits',
+    async () => {
+      const response = await new Octokit().request(`GET ${API_URL}?per_page=1&page=1`, {
+        headers: {
+          authorization: TOKEN
+        },
+      });
+      
+      // the total amount of commits is taken from a response's header
+      return response.headers?.link?.split(";")[1].match(/.*page=(?<page_num>\d+)/)?.groups?.page_num;
+  }
+);
